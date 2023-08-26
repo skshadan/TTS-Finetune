469fa81938eaea3a3023ad1a688fd7b02466e987
EC2 Default User <ec2-user@ip-172-16-71-208.ap-south-1.compute.internal>
2023-08-26 12:57:38 +0000

    SageMaker ready

---
+++ recipes/ljspeech/vits_tts/model/download.py
@@ -0,0 +1,2 @@
+from huggingface_hub import hf_hub_download
+hf_hub_download(repo_id="youmebangbang/vits_tts_models", filename="neil_degrasse_tyson_checkpoint_1910000.pth", local_dir="/home/ec2-user/TTS/recipes/ljspeech/vits_tts/model2")
