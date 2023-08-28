e327356df87a2ab9989a8fc38c74cd9073ff2283
FarziBuilder <112924717+FarziBuilder@users.noreply.github.com>
2023-08-28 19:36:39 +0530

    Update download.py

--- recipes/ljspeech/vits_tts/model/download.py
+++ recipes/ljspeech/vits_tts/model/download.py
@@ -1,2 +1,7 @@
 from huggingface_hub import hf_hub_download
-hf_hub_download(repo_id="youmebangbang/vits_tts_models", filename="neil_degrasse_tyson_checkpoint_1910000.pth", local_dir="/home/ec2-user/TTS/recipes/ljspeech/vits_tts/model2")
+
+# Download the model checkpoint
+hf_hub_download(repo_id="youmebangbang/vits_tts_models", filename="neil_degrasse_tyson_checkpoint_1910000.pth", local_dir="/home/ec2-user/.local/share/tts/tts_models--en--ljspeech--vits")
+
+# Download the config.json file
+hf_hub_download(repo_id="youmebangbang/vits_tts_models", filename="config.json", local_dir="/home/ec2-user/.local/share/tts/tts_models--en--ljspeech--vits")
