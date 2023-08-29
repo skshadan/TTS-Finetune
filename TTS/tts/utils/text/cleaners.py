475bea1ebeb1a251279d6a6a22b9abec19e4eee5
FarziBuilder <112924717+FarziBuilder@users.noreply.github.com>
2023-08-29 12:51:23 +0530

    Update cleaners.py

--- TTS/tts/utils/text/cleaners.py
+++ TTS/tts/utils/text/cleaners.py
@@ -39,9 +39,8 @@ def convert_to_ascii(text):
 
 
 def remove_aux_symbols(text):
-    text = re.sub(r"[\<\>\(\)\[\]\"]+", "", text)
+    text = re.sub(r"<>|\"+", "", text)
     return text
-
 
 def replace_symbols(text, lang="en"):
     """Replace symbols based on the lenguage tag.
