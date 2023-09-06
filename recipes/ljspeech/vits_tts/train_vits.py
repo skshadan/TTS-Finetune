5622151fdcfb415f47fe9878e3b14f93136358d5
Shadan <118248053+skshadan@users.noreply.github.com>
2023-09-06 11:00:53 +0530

    Update train_vits.py
    - Changed the path, configs

--- recipes/ljspeech/vits_tts/train_vits.py
+++ recipes/ljspeech/vits_tts/train_vits.py
@@ -9,45 +9,52 @@ from TTS.tts.models.vits import Vits, VitsAudioConfig
 from TTS.tts.utils.text.tokenizer import TTSTokenizer
 from TTS.utils.audio import AudioProcessor
 
+#output_path = os.path.dirname(os.path.abspath(__file__))
+##########################################
+#Change this to your dataset directory
+##########################################
 output_path = os.path.dirname(os.path.abspath(__file__))
 dataset_config = BaseDatasetConfig(
-    formatter="ljspeech", meta_file_train="metadata.csv", path="/home/ec2-user/TTS/recipes/ljspeech/vits_tts/chinti"
+##########################################
+#Change this to your dataset directory
+##########################################
+    formatter="ljspeech", meta_file_train="metadata.csv", path="/home/ec2-user/SageMaker/tts-sage/recipes/ljspeech/vits_tts/adam"
+
 )
 audio_config = VitsAudioConfig(
-    sample_rate=22050, win_length=1024, hop_length=256, num_mels=80, mel_fmin=0, mel_fmax=None
+    sample_rate=48000, win_length=1024, hop_length=256, num_mels=80, mel_fmin=0, mel_fmax=None
 )
 
-
 config = VitsConfig(
-		audio=audio_config,
-		run_name="new-voice1",
-		batch_size=4,
-		eval_batch_size=4,
-		batch_group_size=5,
-		num_loader_workers=1,
-		num_eval_loader_workers=1,
-		run_eval=True,
-		test_delay_epochs=-1,
-		epochs=1,
-		text_cleaner="phoneme_cleaners",
-		use_phonemes=False,
-		phoneme_language="en-us",
-		phoneme_cache_path=os.path.join(output_path, "phoneme_cache"),
-		compute_input_seq_cache=True,
-		print_step=25,
-		print_eval=True,
-		mixed_precision=False,
-		output_path=output_path,
-		datasets=[dataset_config],
-		cudnn_benchmark=False,
-		test_sentences=[
-		  "Hello, how are you this is a test",
-		  "Hello  guys! i am bheem hope you are doing good",
-		  "This is adam from india, i love india",
-		  "This is just my ai voice, hope  you guys know me",
-		  "Hello, adam is here, how are you all?",
-		],
-	)
+    audio=audio_config,
+    run_name="tts-adam-48k",
+    batch_size=8,
+    eval_batch_size=12,
+    batch_group_size=4,
+#    num_loader_workers=8,
+    num_loader_workers=4,
+    num_eval_loader_workers=4,
+    run_eval=True,
+    test_delay_epochs=-1,
+    epochs=100000,
+    save_step=1000,
+	  save_checkpoints=True,
+	  save_n_checkpoints=4,
+	  save_best_after=1000,
+    #text_cleaner="english_cleaners",
+    text_cleaner="multilingual_cleaners",
+    use_phonemes=True,
+    phoneme_language="en-us",
+    phoneme_cache_path=os.path.join(output_path, "phoneme_cache"),
+    compute_input_seq_cache=True,
+    print_step=25,
+    print_eval=True,
+    mixed_precision=True,
+    output_path=output_path,
+    datasets=[dataset_config],
+    cudnn_benchmark=False,
+)
+
 # INITIALIZE THE AUDIO PROCESSOR
 # Audio processor is used for feature extraction and audio I/O.
 # It mainly serves to the dataloader and the training loggers.
@@ -67,14 +74,13 @@ train_samples, eval_samples = load_tts_samples(
     dataset_config,
     eval_split=True,
     eval_split_max_size=config.eval_split_max_size,
-    #eval_split_size=config.eval_split_size,
-    eval_split_size=0.05555555555555555,
+    eval_split_size=config.eval_split_size,
 )
 
 # init model
 model = Vits(config, ap, tokenizer, speaker_manager=None)
 
-# init the trainer and 🚀
+# init the trainer and begin
 trainer = Trainer(
     TrainerArgs(),
     config,
